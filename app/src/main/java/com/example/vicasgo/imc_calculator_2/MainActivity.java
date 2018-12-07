d20dff0b794340b5f7a065d8a1360fbeee057856
Victor Castro <vicasgo@gmail.com>
2018-12-07 10:11:07 +0000

    cambio de activity

--- app/src/main/java/com/example/vicasgo/imc_calculator_2/MainActivity.java
+++ app/src/main/java/com/example/vicasgo/imc_calculator_2/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.vicasgo.imc_calculator_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btncalc,btnVerData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btncalc = (Button)findViewById(R.id.btnCalcularIMC);
+        btncalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ActivityDataIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
